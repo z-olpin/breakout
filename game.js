c0c89bee2cf863c0ce18329bea7220568199744c
Zachary Olpin <info@zacharyolp.in>
2019-09-29 06:20:30 -0600

    break blocks

--- game.js
+++ game.js
@@ -8,41 +8,24 @@ const width = canvas.width = 800
 const height = canvas.height = 700
 canvas.style.border = '30px solid #a7cdff'
 canvas.style.borderRadius = '20px'
-restart.addEventListener("click", () => location.reload())
-// Event handlers to move paddle
-const keydown = e => {
-  let code = e.keyCode
-  switch (code) {
-    case 37:
-      dir = 'left'
-      break
-    case 39:
-      dir = 'right'
-      break
-    default:
-      break
+
+let ball = {
+  direction: {
+    x: -1,
+    y: -1
+  },
+  velocity: {
+    x: 6,
+    y: 9
+  },
+  position: {
+    x: 500,
+    y: 688
   }
 }
 
-const keyup = () => {
-  dir = null
-}
+let lostGame
 
-document.addEventListener("keydown", keydown)
-document.addEventListener("keyup", keyup)
-
-// Store blocks in 2D array where block -> x-coordinate
-let blocks = [[...Array(10)], [...Array(10)], [...Array(10)], [...Array(10)], [...Array(10)]]
-blocks = blocks.map(row=>row.map((v, i) => (width / 10) * i+2))
-
-let paddleX = 20
-let xDirection = -1
-let yDirection = -1
-let xVelocity = 6
-let yVelocity = 8
-let ballPosition = {x: 500, y: 688}
-let lostGame = false
-let dir = null
 let paddle = {
   moveRight: () => {
     paddle.farLeft += 10
@@ -58,21 +41,50 @@ let paddle = {
     paddle.right -= 10
     paddle.farRight -= 10
   },
+  direction: undefined,
   farLeft: 20,
-  left: 45,
-  center: 70,
-  right: 95,
-  farRight: 120
+  left: 50,
+  center: 80,
+  right: 110,
+  farRight: 140
 }
 
-const drawBlock = (x, y) => {
-  ctx.fillStyle = '#dd7777'
-  ctx.fillRect(x, y, width/10-8, 25)
+// Event handlers to move paddle
+const keydown = e => {
+  let code = e.keyCode
+  switch (code) {
+    case 37:
+      paddle.direction = 'left'
+      break
+    case 39:
+      paddle.direction = 'right'
+      break
+    default:
+      break
+  }
 }
+
+const keyup = () => {
+ paddle.direction = undefined
+}
+
+const drawBlock = (x, y, w, h) => {
+  ctx.fillStyle = '#a7cdff'
+  ctx.fillRect(x, y, w, h)
+  ctx.fillStyle = '#dd7777'
+  ctx.fillRect(x+5, y+5, w-10, h-10)
+  if (x == null) {
+    ctx.fillStyle = '#a7cdff'
+    ctx.fillRect(x, y, w, h)
+  }
+}
+
+// Store blocks in 2D array -> [x-coord, y-coord, width, height]
+let blocks = [...Array(4)].map(e => [...Array(10)])
+blocks = blocks.map((row, j)=>row.map((v, i) => [(width / 10) * i, 40 * j, width/10, 40]))
 
 const draw = () => {
 
-  // Check if lostGame
   if (lostGame) {
     ctx.fillStyle = '#ff0000'
     ctx.fillRect(0, 0, width, height)
@@ -83,79 +95,53 @@ const draw = () => {
     ctx.fillRect(0, 0, width, height)
 
     // Draw paddle
-    if (dir == 'left') paddle.moveLeft()
-    else if (dir == 'right') paddle.moveRight()
+    if (paddle.direction == 'left') paddle.moveLeft()
+    else if (paddle.direction == 'right') paddle.moveRight()
     ctx.fillStyle = '#1a5733'
-    ctx.fillRect(paddle.farLeft, height - 30, 100, 20)
+    ctx.fillRect(paddle.farLeft, height - 30, 120, 20)
     
     // Draw blocks
-    blocks[0].map(x => drawBlock(x, 4))
-    blocks[1].map(x => drawBlock(x, 37))
-    blocks[2].map(x => drawBlock(x, 70))
-    blocks[3].map(x => drawBlock(x, 103))
-    blocks[4].map(x => drawBlock(x, 137))
+    blocks.map(row => row.map(block => drawBlock(...block)))
 
-    // Check if ball hit wall
-    if (ballPosition.y >= height) lostGame = true
-    if (ballPosition.x >= width - 13) xDirection = -1
-    if (ballPosition.x <= 20) xDirection = 1
-    if (ballPosition.y <= 20) yDirection = 1
-   if (ballPosition.x > paddle.farLeft && ballPosition.x < paddle.farRight && ballPosition.y >= height - 52) {
-      if (paddle.farLeft < ballPosition.x && ballPosition.x < paddle.left) {
-        if (xDirection == -1) {
-          xVelocity += 2
-        } else {
-          xVelocity -= 2
+    // :(
+    for (let row = 0; row < blocks.length; row++) {
+      for (let block = 0; block < blocks[row].length; block++) {
+        if (blocks[row][block][0] < ball.position.x && blocks[row][block][1] < ball.position.y && ball.position.x < blocks[row][block][0] + blocks[row][block][2] && ball.position.y < blocks[row][block][1] + blocks[row][block][3]) {
+          if (blocks[row][block][0] != null) {
+            blocks[row][block] = [null, null, null, null]
+            ball.direction.y =  ball.direction.y * -1
+          }
         }
-      if (paddle.left < ballPosition.x && ballPosition.x < paddle.center) {
-        if (xDirection == -1) {
-          xVelocity += 1
-        } else {
-          xVelocity -= 1
-        }
-
-      if (paddle.right < ballPosition.x && ballPosition.x < paddle.farRight) {
-        if (xDirection == -1) {
-          xVelocity += 2
-        } else {
-          xVelocity -= 2
-        }      
       }
-      if (paddle.center < ballPosition.x && ballPosition.x < paddle.right) {
-        if (xDirection == -1) {
-          xVelocity += 1
-        } else {
-          xVelocity -= 1
-        }      
-      }
-      if (xVelocity < 1) xVelocity = 1
-    }
-  }
-      // if (paddle.center < ballPosition.x < paddle.farRight) {
-      //   xVelocity -= 30
-      //   console.log('asdfasdfa')
-
-      // }
-        console.log(ballPosition.x, xVelocity, xDirection)
-      yDirection = -1
     }
 
+    // Check if ball has hit something
+    if (ball.position.y >= height) lostGame = true
+    if (ball.position.x >= width - 13) ball.direction.x = -1
+    if (ball.position.x <= 20) ball.direction.x = 1
+    if (ball.position.y <= 20) ball.direction.y = 1
+    if (ball.position.x > paddle.farLeft &&
+        ball.position.x < paddle.farRight &&
+        ball.position.y >= height - 52) ball.direction.y = -1
+    if (ball.position.y >= height) lostGame = true
 
     // Draw ball
-    ctx.fillStyle = '#ffffff'
     ctx.beginPath()
-    ctx.ellipse(ballPosition.x, ballPosition.y, 15, 15, 0, 0, 2 * Math.PI)
+    ctx.fillStyle = '#ffffff'
+    ctx.ellipse(ball.position.x, ball.position.y, 15, 15, 0, 0, 2 * Math.PI)
     ctx.closePath()
     ctx.fill()
 
     // Set ball position for next render
-    ballPosition.x += xDirection * xVelocity
-    ballPosition.y += yDirection * yVelocity
+    ball.position.x += ball.direction.x * ball.velocity.x
+    ball.position.y += ball.direction.y * ball.velocity.y
 
     requestAnimationFrame(draw)
   }
 }
 requestAnimationFrame(draw)
 
-
+restart.addEventListener("click", () => location.reload())
+document.addEventListener("keydown", keydown)
+document.addEventListener("keyup", keyup)
 
