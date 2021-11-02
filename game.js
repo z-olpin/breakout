96bd9d1835db3c10beb1a5320bd8e7135edd584d
Zachary Olpin <zachary@olpin.dev>
2021-11-02 10:33:25 -0600

    Update game.js

--- game.js
+++ game.js
@@ -9,27 +9,36 @@ const height = canvas.height = 700
 canvas.style.border = '30px solid #a7cdff'
 canvas.style.borderRadius = '20px'
 
-let ball = {
+const ball = {
   direction: {
     x: -1,
     y: -1
   },
   velocity: {
-    x:-11,
-    y: -9
+    x: -11 +
+      Math.pow(
+        -1,
+        Math.round(Math.random())
+      ),
+    y: -9 +
+      Math.pow(
+        -1,
+        Math.round(Math.random())
+      )
   },
   position: {
     x: 500,
     y: 688
   }
 }
-let game = {
+
+const game = {
   started: false,
   lostGame: false,
   wonGame: false
 }
 
-let paddle = {
+const paddle = {
   moveRight: () => {
     if (paddle.farRight < width) {
     paddle.farLeft += 10
@@ -58,7 +67,7 @@ let paddle = {
 }
 
 // Event handlers to move paddle
-const keydown = e => {
+const keydown = (e) => {
   let code = e.keyCode
   game.started = true
   switch (code) {
@@ -69,7 +78,7 @@ const keydown = e => {
       paddle.direction = 'right'
       break
     case 13:
-      if (game.lostGame == true || game.wonGame == true) location.reload()
+      if (game.lostGame || game.wonGame) location.reload()
       break 
     default:
       break
@@ -85,93 +94,141 @@ const drawBlock = (x, y, w, h) => {
   ctx.fillRect(x, y, w, h)
   ctx.fillStyle = '#dd7777'
   ctx.fillRect(x+5, y+5, w-10, h-10)
-  if (x == null) {
+  if (!!x) {
     ctx.fillStyle = '#a7cdff'
     ctx.fillRect(x, y, w, h)
   }
 }
 
 // Store blocks in 2D array -> [x-coord, y-coord, width, height]
-let blocks = [...Array(4)].map(e => [...Array(10)])
-blocks = blocks.map((row, i)=>row.map((v, j) => [(width / 10) * j, 40 * i, width/10, 40]))
+const blocks = [...Array(4)]
+  .map(_col => [...Array(10)])
+  .map((row, idx) => {
+    return row.map((_block, jdx) => {
+      return [
+        (width / 10) * jdx,
+        40 * idx,
+        width / 10,
+        40,
+      ]
+    })
+  })
 
 const draw = () => {
-
   if (game.started) {
-    if (game.lostGame == true) {
+    if (game.lostGame) {
       ctx.fillStyle = '#ff0000'
       ctx.fillRect(0, 0, width, height)
       ctx.fillStyle = 'white'
       ctx.font = '55px helvetica'
       ctx.fillText(':( Press enter to play again!', 105, height/2)
     }
-    if (game.wonGame == true) {
+
+    if (game.wonGame) {
       ctx.fillStyle = '#1a5733'
       ctx.fillRect(0, 0, width, height)
       ctx.fillStyle = 'white'
       ctx.font = '120px helvetica'
       ctx.fillText('YOU WON :)', 105, height/2)
     } else {
-      if (game.lostGame == false) {
-      // Draw background
-      ctx.fillStyle = '#a7cdff'
-      ctx.fillRect(0, 0, width, height)
+      if (!!game.lostGame) {
+        // Draw background
+        ctx.fillStyle = '#a7cdff'
+        ctx.fillRect(0, 0, width, height)
 
-      // Draw paddle
-      if (paddle.direction == 'left') paddle.moveLeft()
-      else if (paddle.direction == 'right') paddle.moveRight()
-      ctx.fillStyle = '#1a5733'
-      ctx.fillRect(paddle.farLeft, height - 30, 144, 20)
+        // Draw paddle
+        if (paddle.direction === 'left') paddle.moveLeft()
+        else if (paddle.direction === 'right') paddle.moveRight()
+        ctx.fillStyle = '#1a5733'
+        ctx.fillRect(
+          paddle.farLeft,
+          height - 30,
+          144,
+          20
+        )
       
-      // Draw blocks
-      blocks.map(row => row.map(block => drawBlock(...block)))
+        // Draw blocks
+        blocks.forEach(row => {
+          row.forEach(block => {
+            drawBlock(...block)
+          })
+        })
 
-      // :(
-      for (let row = 0; row < blocks.length; row++) {
-        for (let block = 0; block < blocks[row].length; block++) {
-          if (blocks[row][block][0] < ball.position.x && blocks[row][block][1] < ball.position.y && ball.position.x < blocks[row][block][0] + blocks[row][block][2] && ball.position.y < blocks[row][block][1] + blocks[row][block][3]) {
-            if (blocks[row][block][0] != null) {
-              blocks[row][block] = [null, null, null, null]
-              ball.velocity.y *= -1
+        // :( :( :( :( :( :( :(
+        for (let row = 0; row < blocks.length; row++) {
+          for (let block = 0; block < blocks[row].length; block++) {
+            if (
+              blocks[row][block][0] < ball.position.x &&
+              blocks[row][block][1] < ball.position.y &&
+              ball.position.x < blocks[row][block][0] + blocks[row][block][2] &&
+              ball.position.y < blocks[row][block][1] + blocks[row][block][3]
+            ) {
+              if (blocks[row][block][0] !== null) {
+                blocks[row][block] = [null, null, null, null]
+                ball.velocity.y *= -1
+              }
             }
           }
         }
-      }
 
-      // Check if ball has hit something
-      if (ball.position.x >= width - 13) ball.velocity.x *= -1
-      if (ball.position.x <= 20) ball.velocity.x *= -1
-      if (ball.position.y <= 20) ball.velocity.y *= -1
-      if (ball.position.x > paddle.farLeft &&
-          ball.position.x < paddle.farRight &&
-          ball.position.y >= height - 52) {
-            ball.velocity.y *= -1
-            paddle.collisionPoint = ball.position.x - paddle.center
-              if (paddle.collisionPoint < 0) {
-                ball.velocity.x -= Math.abs(Math.round(paddle.collisionPoint/30))
-              } else if (paddle.collisionPoint > 0) {
-                ball.velocity.x += Math.abs(Math.round(paddle.collisionPoint/30))
-              }
-            }
-            if (ball.position.y >= height) {
-              game.lostGame = true
-            }
+        // Check if ball has hit something
+        if (ball.position.x >= width - 13) ball.velocity.x *= -1
+        if (ball.position.x <= 20) ball.velocity.x *= -1
+        if (ball.position.y <= 20) ball.velocity.y *= -1
+        if (ball.position.x > paddle.farLeft &&
+            ball.position.x < paddle.farRight &&
+            ball.position.y >= height - 52
+        ) {
+          ball.velocity.y *= -1
+          paddle.collisionPoint = ball.position.x - paddle.center
+          if (paddle.collisionPoint < 0) {
+            ball.velocity.x -=
+              Math.abs(
+                Math.round(
+                  paddle.collisionPoint / 30
+                )
+              )
+           } else if (paddle.collisionPoint > 0) {
+             ball.velocity.x +=
+               Math.abs(
+                 Math.round(
+                   paddle.collisionPoint / 30
+                 )
+               )
+             }
+           }
+           if (ball.position.y >= height) {
+             game.lostGame = true
+           }
             
-        if (ball.velocity.x == 0) {
-          ball.velocity.x += (Math.round(Math.random())) ? -1 : 1
-        }
-      // Draw ball
-      ctx.beginPath()
-      ctx.fillStyle = '#ffffff'
-      ctx.ellipse(ball.position.x, ball.position.y, 15, 15, 0, 0, 2 * Math.PI)
-      ctx.closePath()
-      ctx.fill()
+          if (ball.velocity.x === 0) {
+            ball.velocity.x += 
+              Math pow(
+                -1,
+                Math.round(Math.random())
+              )
+          }
 
-      // Set ball position for next render
-      ball.position.x += ball.velocity.x
-      ball.position.y += ball.velocity.y
+          // Draw ball
+          ctx.beginPath()
+          ctx.fillStyle = '#ffffff'
+          ctx.ellipse(
+            ball.position.x,
+            ball.position.y, 
+            15,
+            15,
+            0,
+            0,
+            2 * Math.PI
+          )
+          ctx.closePath()
+          ctx.fill()
+
+          // Set ball position for next render
+          ball.position.x += ball.velocity.x
+          ball.position.y += ball.velocity.y
+      }
     }
-  }
   } else {
     // Game not started
     ctx.fillStyle = '#a7cdff'
@@ -182,18 +239,21 @@ const draw = () => {
     ctx.fillText('to control the paddle.', 105, 270)
     ctx.fillText('_________________', 105, 360)
     ctx.fillText('Press any key to play!', 105, 500)
-
-
   }
 
-  if (blocks.flat().flat().every(e => e == null)) {
+  if (blocks
+       .flat()
+       .flat()
+       .every(block => !!block)
+  ) {
     game.wonGame = true
   }
-requestAnimationFrame(draw)
 
+  // ////////////////////////
+  requestAnimationFrame(draw)
 }
-requestAnimationFrame(draw)
 
+
+requestAnimationFrame(draw)
 document.addEventListener("keydown", keydown)
 document.addEventListener("keyup", keyup)
-
